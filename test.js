0293690698a84279cb67f3f90168c546296374f3
ozzieem <ozzieee@gmail.com>
2017-01-28 01:43:41 +0100

    add gametime tracking

--- test.js
+++ test.js
@@ -1,97 +1,98 @@
 var Discordie;
-try { Discordie = require('discordie'); } catch(e) {}
+try { Discordie = require('discordie'); } catch (e) { }
 
 
-const client = new Discordie({autoReconnect: true});
+const client = new Discordie({ autoReconnect: true });
 const Events = Discordie.Events;
-
 
 client.connect({
 	token: 'Mjc0NTU4NjY1NjQwNjQwNTEy.C2z27A.JUGs7m0PgMSYTFZgaO-02ZGIpFc'
 });
 
-var fuckyou = ". \n" +
-"........................./´¯/) \n" + 
-"......................,/¯..// \n" +
-"...................../..../ / \n" +
-"............./´¯/'...'/´¯¯`·¸ \n" +
-"........../'/.../..../......./¨¯\ \n" +
-"........('(...´(..´......,~/'...') \n" +
-"........................../..../ \n" +
-"..........''............. _.·´ \n" +
-"..........................( \n" +
-"...........................\\ \n";
 
-var commands = "!fu\n!stupid bot\n!shit\n!poop\n";
+var serverUsers = {};
 
+// ---------------- BOT-SETUP ---------------------
 
 client.Dispatcher.on(Events.GATEWAY_READY, e => {
-	client.User.username = "ShitBot";
 	console.log('Connected as: ' + client.User.username);
+
+	console.log("Adding all users on server");
+	client.Users.forEach(function (e) {
+		serverUsers[e.username] = new UserClass(e.username);
+	});
+	console.log("Users added: " + Object.keys(serverUsers).length);
 });
 
+// ---------------- MESSAGE-UPDATES  ---------------------
+
 client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
-	console.log(e);
 	var content = e.message.content;
 	var message;
-	if(content == '.fu') {
-		message = fuckyou;
+	if (content == '.fu') {
+		respondToUserCommand(e, fuckyou);
 	}
-	if(content == ".stupid bot") {
-		message = "No u!";
+	if (content == ".stupid bot") {
+		respondToUserCommand(e, "No u!");
 	}
-	if(content == ".shit" || content == "!poop") {
-		message = ":poop:";
+	if (content == ".shit") {
+		respondToUserCommand(e, ":poop:");
 	}
-	if(content == ".help") {
-		message = "GTFO"
+	if (content == ".help") {
+		respondToUserCommand(e, "GTFO");
 	}
-	if(content == ".commands") {
-		message = commands;
+	if (content == ".commands") {
+		respondToUserCommand(e, commands);
 	}
-	
-	e.message.channel.sendMessage(message);
 });
+
+// ---------------- USER-UPDATES  ---------------------
 
 client.Dispatcher.on(Events.PRESENCE_UPDATE, e => {
 
 	var status = e.user.status;
 	var prevStatus = e.user.previousStatus;
-	var channel;
 
 	if (status != prevStatus) {
-		channel = getChannel("bottesting");
-		if(e.user.username == "Coffeefox") {
-			if(status == "online" && prevStatus == "offline") {
-				channel.sendMessage("Hej Mikael!");
-			} 
-			else if(status == "offline" && prevStatus == "online") {
-				channel.sendMessage("Hejdå Mikael!");
-			}
+		if (status == "online" && prevStatus == "offline") {
+			console.log("[DEBUG] User logged in: " + serverUsers[e.user.username].name);
+		}
+		else if (status == "offline" && prevStatus == "online") {
+			console.log("[DEBUG] User disconnected: " + serverUsers[e.user.username].name);
 		}
 	}
-
-	console.log(status);
-	console.log(prevStatus);
 
 	var game = e.user.gameName;
 	var prevGame = e.user.previousGameName;
-	
-	channel = getChannel("bottesting");
-	
+
+	var channel = getChannel("bottesting");
+
 	if (game != prevGame) {
 		var message;
-		if(game != null) {
-			message = e.user.username + " started playing " + game;
-		} else if(prevGame != null) {
-			message = e.user.username + " stopped playing " + prevGame;
+		var user = serverUsers[e.user.username];
+		if (game != null) {
+			user.startGame();
+			message = user.name + " started playing " + game;
+			console.log(message);
+		} else if (prevGame != null) {
+			if (user.gameTime.start != 0) {
+				user.endGame();
+				message = user.name + " stopped playing " + prevGame + ". Duration: " + user.gameTime.total + " s";
+			}
+			else {
+				message = user.name + " stopped playing " + prevGame;
+			}
+			console.log(message);
 		}
 		channel.sendMessage(message);
 	}
-
-	console.log(game);
-	console.log(prevGame);
 });
+
+// ---------------- FUNCTIONS ---------------------
+function respondToUserCommand(event, msg) {
+	console.log("Sent message: " + msg + " to channel: " + event.message.channel.name)
+	event.message.channel.sendMessage(msg);
+}
 
 function getChannel(cname) {
 	return client.Channels.filter(c => (c.name == cname))[0];
@@ -100,3 +101,58 @@ function getChannel(cname) {
 function getUser(uname) {
 	return client.Users.filter(u => (u.username == uname))[0];
 }
+// ------------------------------------------------
+
+// ---------------- CLASSES -----------------------
+class UserClass {
+	constructor(name) {
+		this.name = name;
+		this.gameTime = new Time();
+	};
+
+	startGame() {
+		this.gameTime.startTimer();
+	}
+
+	endGame() {
+		this.gameTime.calculateTotalTime();
+	}
+}
+
+class Time {
+	constructor() {
+		this.start = 0;
+		this.end = 0;
+		this.total = 0;
+	};
+
+	startTimer() {
+		this.start = new Date();
+	};
+
+	endTimer() {
+		this.end = new Date();
+	};
+
+	calculateTotalTime() {
+		this.endTimer();
+		this.total = (this.end.getTime() - this.start.getTime()) / 1000;
+		console.log('[DEBUG] Duration: ' + this.total + ' sec');
+	};
+}
+// ------------------------------------------------
+
+
+const fuckyou = ". \n" +
+	"........................./´¯/) \n" +
+	"......................,/¯..// \n" +
+	"...................../..../ / \n" +
+	"............./´¯/'...'/´¯¯`·¸ \n" +
+	"........../'/.../..../......./¨¯\ \n" +
+	"........('(...´(..´......,~/'...') \n" +
+	"........................../..../ \n" +
+	"..........''............. _.·´ \n" +
+	"..........................( \n" +
+	"...........................\\ \n";
+
+const commands = "\n!fu\n!stupid bot\n!shit\n";
