8529b26181e6affb8a55e6d6b43f7d174c88b1aa
ozzieem <ozzieee@gmail.com>
2017-01-30 18:04:18 +0100

    add user status and registered commands

--- test.js
+++ test.js
@@ -10,25 +10,28 @@ client.connect({
 });
 
 
-var serverUsers = {};
-
 // ---------------- BOT-SETUP ---------------------
 
-client.Dispatcher.on(Events.GATEWAY_READY, e => {	
-	console.log('[LOG] Connected as: ' + client.User.username);
+client.Dispatcher.on(Events.GATEWAY_READY, e => {
+	TimeLog.log("Connected as: " + client.User.username);
 
-	console.log("[LOG] Adding all users on server");
-	client.Users.forEach(function (e) {
-		serverUsers[e.username] = new UserClass(e.username);
-		
-	});
-	console.log("[LOG] Users added: " + Object.keys(serverUsers).length);
+	ServerUsers.create();
+
+	// Receiveing online/offline users on server
+	const guild = client.Guilds.find(g => g.name == "T_CONNECT");
+
+	addUsersOnServer(guild);
+
+	ServerUsers.ToString();
 });
 
 // ---------------- MESSAGE-UPDATES ---------------------
 
 client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 	var content = e.message.content;
+
+	TimeLog.debug("User " + e.username + " sent message " + content);
+
 	var message;
 	if (content == '.fu') {
 		respondToUserCommand(e, fuckyou);
@@ -45,36 +48,60 @@ client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 	if (content == ".commands") {
 		respondToUserCommand(e, commands);
 	}
-	if(content.substr(0, content.indexOf(' ')) == ".rust") {
+	// TODO: Abstract this substr-shit into something else
+	if (content.substr(0, content.indexOf(' ')) == ".rust") {
 		var link = "http://rust.wikia.com/wiki/";
 		var search_item = content.substr(content.indexOf(' ') + 1);
 		link += search_item;
 		respondToUserCommand(e, link);
 	}
-	if(content == ".coffee") {
+	// TODO: Same shit here with substr
+	if (content.substr(0, content.indexOf(' ')) == ".status") {
+		var usrname = content.substr(content.indexOf(' ') + 1);
+		try {
+			respondToUserCommand(e, ServerUsers.get(usrname).getStatus());
+		} catch (err) {
+			respondToUserCommand(e, "Username " + usrname + " does not exist, you shitter");
+		}
+	}
+	if (content.substr(0, content.indexOf(' ')) == ".registered") {
+		var usrname = content.substr(content.indexOf(' ') + 1);
+		try {
+			respondToUserCommand(e, ServerUsers.get(usrname).getRegistered());
+		} catch (err) {
+			respondToUserCommand(e, "Username " + usrname + " does not exist, you shitter");
+		}
+
+	}
+	if (content == ".coffee") {
 		respondToUserCommand(e, "Grinding coffee-beans...");
-		setTimeout(function() { respondToUserCommand(e, "Placing ground coffee in filter...") }, 3000);
-		setTimeout(function() { respondToUserCommand(e, "Brewing coffee...") }, 5000);
-		setTimeout(function() { respondToUserCommand(e, "Enjoy your shitty coffee. :poop: :coffee:") }, 10000);
+		setTimeout(function () { respondToUserCommand(e, "Placing ground coffee in filter...") }, 3000);
+		setTimeout(function () { respondToUserCommand(e, "Brewing coffee...") }, 5000);
+		setTimeout(function () { respondToUserCommand(e, "Enjoy your shitty coffee. :poop: :coffee:") }, 10000);
 	}
-	if(content == ".codeblogs") {
-		
+	if (content == ".codeblogs") {
+
 	}
+
 });
 
 // ---------------- USER-UPDATES  ---------------------
 
 client.Dispatcher.on(Events.PRESENCE_UPDATE, e => {
 
+	var user = ServerUsers.get(e.user.username);
+
 	var status = e.user.status;
 	var prevStatus = e.user.previousStatus;
 
 	if (status != prevStatus) {
 		if (status == "online" && prevStatus == "offline") {
-			console.log("[LOG] User logged in: " + serverUsers[e.user.username].name);
+			user.setOnline();
+			TimeLog.log("User logged in: " + ServerUsers.get(e.user.username).name)
 		}
 		else if (status == "offline" && prevStatus == "online") {
-			console.log("[LOG] User disconnected: " + serverUsers[e.user.username].name);
+			user.setOffline();
+			TimeLog.log("User disconnected: " + ServerUsers.get(e.user.username).name);
 		}
 	}
 
@@ -85,12 +112,9 @@ client.Dispatcher.on(Events.PRESENCE_UPDATE, e => {
 
 	if (game != prevGame) {
 		var message;
-		var user = serverUsers[e.user.username];
-
 		if (checkUserRole(e.member.roles, "Master")) {
 			if (game != null) {
 				message = user.startGame(game);
-				console.log(message);
 			} else if (prevGame != null) {
 				if (user.gameTime.start != 0) {
 					message = user.endGame(prevGame);
@@ -98,8 +122,8 @@ client.Dispatcher.on(Events.PRESENCE_UPDATE, e => {
 				else {
 					message = user.name + " stopped playing " + prevGame;
 				}
-				console.log("[LOG] " + message);
 			}
+			TimeLog.log(message);
 			channel.sendMessage(message)
 		};
 	}
@@ -107,34 +131,165 @@ client.Dispatcher.on(Events.PRESENCE_UPDATE, e => {
 
 
 // ---------------- FUNCTIONS ---------------------
-function respondToUserCommand(event, msg) {
-	console.log("[LOG] Sent message: " + msg + " to: " + event.message.channel.name);
-	event.message.channel.sendMessage(msg);
-}
+{
+	function addUsersOnServer(guild) {
 
-function checkUserRole(roles, rname) {
-	var role = roles.filter(r => (r.name == rname))[0];
-	if(role != undefined) {
-		return true;
+		var onlineUsers = client.Users.onlineMembersForGuild(guild);
+		var offlineUsers = client.Users.offlineMembersForGuild(guild);
+
+		TimeLog.log("Adding all users on server");
+		onlineUsers.forEach(function (onlineUser) {
+			ServerUsers.add(
+				onlineUser.username,
+				new UserClass(
+					onlineUser.username,
+					onlineUser.status,
+					onlineUser.registeredAt
+				)
+			);
+		});
+		TimeLog.log("Online users added: " + onlineUsers.length);
+
+		offlineUsers.forEach(function (offlineUser) {
+			ServerUsers.add(
+				offlineUser.username,
+				new UserClass(
+					offlineUser.username,
+					offlineUser.status,
+					offlineUser.registeredAt
+				)
+			);
+		});
+		TimeLog.log("Offline users added: " + offlineUsers.length);
+		TimeLog.log("Total users added: " + ServerUsers.size());
 	}
-	return false;
 
-}
+	function respondToUserCommand(event, msg) {
+		TimeLog.log("Sent message: " + msg + " to: " + event.message.channel.name);
+		event.message.channel.sendMessage(msg);
+	}
 
-function getChannel(cname) {
-	return client.Channels.filter(c => (c.name == cname))[0];
-}
+	function checkUserRole(roles, rname) {
+		var role = roles.filter(r => (r.name == rname))[0];
+		if (role != undefined) {
+			return true;
+		}
+		return false;
+	}
 
-function getUser(uname) {
-	return client.Users.filter(u => (u.username == uname))[0];
+	function getChannel(cname) {
+		return client.Channels.filter(c => (c.name == cname))[0];
+	}
+
+	function getUser(uname) {
+		return client.Users.filter(u => (u.username == uname))[0];
+	}
 }
 // ------------------------------------------------
 
 // ---------------- CLASSES -----------------------
+{ // command classes
+	const commands = ".\n.fu\n.stupid bot\n.shit\n.rust <item>\n.coffee\n.status <username>\n.registered <username>";	
+	{	// Command classes
+		/**
+		 * Command container
+		 */
+		class CommandList {
+			constructor() {
+				this.cmds = {};
+			}
+
+			add(cmd_obj) {
+				this.cmds[cmd_obj.cmd] = cmd_obj;
+			}
+
+			addAll() {
+			}
+		}
+
+		/**
+		 * A command class to store a specific command
+		 */
+		class Command {
+			// static fu_cmd = '.fu';
+			// static stupidbot_cmd = '.stupid bot';
+			// static shit_cmd = '.shit';
+			// static rust_cmd = '.rust';
+			// static coffee_cmd = '.coffee';
+
+			fu() {
+				return fuckyou;
+			}
+
+			stupidbot() {
+				return "No. I'm a shitty bot. Get it right, you little bitch.";
+			}
+
+			shit() {
+				return ":poop:";
+			}
+
+			rust(search_word) {
+				var link = "http://rust.wikia.com/wiki/";
+				var search_item = search_word.substr(search_word.indexOf(' ') + 1);
+				link += search_item;
+				return link;
+			}
+
+			help() {
+				return "Computer says No.";
+			}
+
+			commands() {
+				// TODO: simple way of listing commands
+				// return commands;
+			}
+		}
+	}
+}
+
+/**
+ * Static class-container for users on server
+ */
+class ServerUsers {
+	static create() {
+		this.dict = {};
+	}
+
+	static add(username, user) {
+		this.dict[username] = user;
+	}
+
+	static get(username) {
+		return this.dict[username];
+	}
+
+	static size() {
+		return Object.keys(this.dict).length;
+	}
+
+	static ToString() {
+		console.log("Printing all ServerUsers: \n");
+		for (var key in this.dict) {
+			if (this.dict.hasOwnProperty(key)) {
+				console.log(this.dict[key]);
+			}
+		}
+	}
+}
+
+
+/**
+ * Custom class for a user
+ */
 class UserClass {
-	constructor(name) {
+	constructor(name, status, registered) {
 		this.name = name;
 		this.gameTime = new Time();
+		this.loggedOnTime = Time.now();
+		this.loggedOffTime = "Undetected";
+		this.status = status;
+		this.registered = registered;
 	};
 
 	startGame(game) {
@@ -145,14 +300,52 @@ class UserClass {
 	endGame(prevGame) {
 		return this.name + " stopped playing " + prevGame + ". " + this.gameTime.calculateTotalTime();
 	}
+
+	setOnline() {
+		this.loggedOnTime = Time.now();
+		TimeLog.debug("User " + this.name + " loggedOnTime set to - " + this.loggedOnTime);
+	}
+
+	setOffline() {
+		this.loggedOffTime = Time.now();
+		TimeLog.debug("User " + this.name + " loggedOffTime set to - " + this.loggedOffTime);
+	}
+
+	getStatus() {
+		return (
+			"Status for " + this.name + "\n" +
+			"Online: " + this.loggedOnTime + "\n" +
+			"Offline: " + this.loggedOffTime
+		);
+	}
+
+	getRegistered() {
+		return this.name + " registered at " + this.registered;
+	}
+
+	ToString() {
+		return this.name + " : " + this.status + " : " + this.registered + "\n";
+	}
 }
 
+/**
+ * Custom class for time-managing
+ */
 class Time {
 	constructor() {
 		this.start = 0;
 		this.end = 0;
 		this.duration = 0;
 	};
+
+	static now() {
+		var d = new Date();
+		var h = (d.getHours() < 10 ? '0' : '') + d.getHours();
+		var m = (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
+		var s = (d.getSeconds() < 10 ? '0' : '') + d.getSeconds();
+
+		return h + ':' + m + ':' + s;
+	}
 
 	startTimer() {
 		this.start = new Date();
@@ -185,6 +378,19 @@ class Time {
 		return time_str;
 	};
 }
+
+/**
+ * Class for printing messages to console with time
+ */
+class TimeLog {
+	static debug(text) {
+		console.log("[DEBUG] (" + Time.now() + ") " + text);
+	}
+
+	static log(text) {
+		console.log("[LOG] (" + Time.now() + ") " + text);
+	}
+}
 // ------------------------------------------------
 
 
@@ -199,5 +405,3 @@ const fuckyou = ". \n" +
 	"..........''............. _.·´ \n" +
 	"..........................( \n" +
 	"...........................\\ \n";
-
-const commands = ".\n.fu\n.stupid bot\n.shit\n.rust <item>\n.coffee\n";
